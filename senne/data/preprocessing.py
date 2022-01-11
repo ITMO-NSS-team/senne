fix normalization

senne/data/preprocessing.py
@@ -1,4 +1,3 @@
-import cv2 as cv
 import numpy as np
 
 from senne.log import senne_logger
@@ -38,7 +37,9 @@ def normalize(features_tensor: np.array, target_tensor: np.array):
 
         min_value = float(np.min(band_tensor))
         max_value = float(np.max(band_tensor))
-        features_tensor[:, band_id, :, :] = cv.normalize(band_tensor, min_value, max_value)
+        features_tensor[:, band_id, :, :] = min_max_normalize(band_tensor,
+                                                              min_value,
+                                                              max_value)
 
         # Save information about transformations
         band_boundaries.update({band_id: {'min': min_value, 'max': max_value}})
@@ -49,12 +50,30 @@ def normalize(features_tensor: np.array, target_tensor: np.array):
 def apply_normalization(features_tensor: np.array, preprocessing_info: dict):
     """ Perform normalization procedure for new unseen data """
     n_objects, n_bands, n_rows, n_columns = features_tensor.shape
+    features_tensor = np.array(features_tensor, dtype='float32')
 
     for band_id in range(n_bands):
         band_tensor = features_tensor[:, band_id, :, :]
         min_value = preprocessing_info['info'][str(band_id)]['min']
         max_value = preprocessing_info['info'][str(band_id)]['max']
 
-        features_tensor[:, band_id, :, :] = cv.normalize(band_tensor, min_value, max_value)
+        features_tensor[:, band_id, :, :] = min_max_normalize(band_tensor,
+                                                              min_value,
+                                                              max_value)
 
     return features_tensor
+
+
+def min_max_normalize(matrix: np.array, min_value: float, max_value: float):
+    """
+    Perform normalization procedure to scale values in images to range
+    from -0.5 to 0.5
+
+    :param matrix: matrix with shape (n x rows x cols) for normalization
+    :param min_value: min value for range
+    :param max_value: max value for range
+    """
+    matrix_to_process = matrix[0, :, :]
+    scaled_m = ((matrix_to_process - min_value) / (max_value - min_value)) - 0.5
+
+    return np.expand_dims(scaled_m, axis=0)